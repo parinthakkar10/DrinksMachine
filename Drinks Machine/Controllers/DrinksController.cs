61e22aaaf2185efc964a6cc9ff356a8cd0f93b78
version <rails -version>
2021-05-20 16:31:33 -0400

    Add project files.

---
+++ Drinks Machine/Controllers/DrinksController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Drinks_Machine.Models;
+
+namespace Drinks_Machine.Controllers
+{
+    public class DrinksController : Controller
+    {
+        Dictionary<string, int> drinksList = new Dictionary<string, int>();
+
+        public static int coke_count = 5;
+
+        public static int coke_value = 25;
+        
+        public static int pepsi_count = 15;
+
+        public static int pepsi_value = 36;
+
+        public static int soda_count = 3;
+
+        public static int soda_value = 45;
+
+        public static int cents_count = 100;
+
+        public static int centsValue = 10;
+
+        public static int penny_count = 10;
+
+        public static int pennyValue = 1;
+
+        public static int nickel_count = 5;
+
+        public static int nickelValue = 5;
+
+        public static int quarter_count = 25;
+
+        public static int quarterValue = 25;
+        public DrinksController()
+        {
+            drinksList.Add("Coke", coke_count);
+            drinksList.Add("Pepsi", pepsi_count);
+            drinksList.Add("Soda", soda_count);
+            drinksList.Add("Cents", cents_count);
+            drinksList.Add("Penny", penny_count);
+            drinksList.Add("Nickels", nickel_count);
+            drinksList.Add("Quarter", quarter_count);
+        }
+        public IActionResult Index()
+        {
+            Drinks drinksModel = new Drinks();
+            drinksModel.DrinksList = drinksList;
+            drinksModel.CentsValue = centsValue;
+            drinksModel.PennyValue = pennyValue;
+            drinksModel.NickelsValue = nickelValue;
+            drinksModel.QuarterValue = quarterValue;
+            drinksModel.CokeValue = coke_value;
+            drinksModel.PepsiValue = pepsi_value;
+            drinksModel.SodaValue = soda_value;
+            return View(drinksModel);
+        }
+
+        [HttpPost]
+        public IActionResult Purchase(Drinks drinksModel)
+        {
+            int cokeQty = drinksModel.CokeQty;
+            int pepsiQty = drinksModel.PepsiQty;
+            int sodaQty = drinksModel.SodaQty;
+            coke_count -= cokeQty;
+            pepsi_count -= pepsiQty;
+            soda_count -= sodaQty;
+            cents_count -= drinksModel.Cents;
+            penny_count -= drinksModel.Penny;
+            nickel_count -= drinksModel.Nickels;
+            quarter_count -= drinksModel.Quarter;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Drinks Machine/Models/Drinks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Drinks_Machine.Models
+{
+    public class Drinks
+    {
+        //public static int coke_count = 5;
+
+        //public static int pepsi_count = 15;
+
+        //public static int soda_count = 3;
+        public int CokeQty { get; set; }
+        public int CokeValue { get; set; }
+        public int PepsiQty { get; set; }
+        public int PepsiValue { get; set; }
+        public int SodaQty { get; set; }
+        public int SodaValue { get; set; }
+        public int CentsValue { get; set; }
+        public int Cents { get; set; }
+        public int PennyValue { get; set; }
+        public int Penny { get; set; }
+        public int NickelsValue { get; set; }
+        public int Nickels { get; set; }
+        public int QuarterValue { get; set; }
+        public int Quarter { get; set; }
+        public Dictionary<string, int> DrinksList { get; set; }
+    }
+}
